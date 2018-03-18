47367d82400bb034987c92d7643e469e39276372
MattDAVM <37452599+MattDAVM@users.noreply.github.com>
2018-03-18 13:26:59 -0300

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+ 
+ 
+ $(document).ready(function(){  
+	
+	$("#topPresentation").animate(
+	{
+		left: '300px', 
+		opacity: '1' 
+	}, 
+		2000
+	); 
+	
+	console.log($( "body" ).scrollTop());
+	
+     
+	
+});
+ 
